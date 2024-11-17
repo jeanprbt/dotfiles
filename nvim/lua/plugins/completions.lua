c62bb13643c36459fba01248cbe3288d27407c70
Jean Perbet <jeanperbet@icloud.com>
2024-11-17 16:27:23 +0100

    Add pdf handler binary & CSV support
    Add rainbow CSV extension to support CSV files in nvim. Include `tdf` binary
    on the repo for easier later usage.

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -88,6 +88,14 @@ return {
 			formatting = {
 				format = lspkind.cmp_format(),
 			},
+			enabled = function()
+				local context = require("cmp.config.context")
+				if vim.api.nvim_get_mode().mode == "c" then
+					return true
+				else
+					return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+				end
+			end,
 		}
 	end,
 }
