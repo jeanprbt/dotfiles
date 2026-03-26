04924c45503e1e6a9735e6861d82fe3a3fbaf651
Jean Perbet <jean.perbet@mistral.ai>
2026-03-26 09:21:19 +0100

    Simplify bottom line
    Remove symbols & filetypes to avoid overloaded lualine.

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -98,7 +98,6 @@ return {
                             end,
                             icon = { "󰛖", color = { fg = pink } },
                         },
-                        "filetype",
                     },
                     lualine_y = {
                         { "progress", padding = { left = 1, right = 1 } },
@@ -109,26 +108,6 @@ return {
                 },
                 extensions = { "neo-tree", "lazy", "fzf" },
             }
-
-            -- do not add trouble symbols if aerial is enabled
-            -- And allow it to be overriden for some buffer types (see autocmds)
-            if vim.g.trouble_lualine and LazyVim.has("trouble.nvim") then
-                local trouble = require("trouble")
-                local symbols = trouble.statusline({
-                    mode = "symbols",
-                    groups = {},
-                    title = false,
-                    filter = { range = true },
-                    format = "{kind_icon}{symbol.name:Normal}",
-                    hl_group = "lualine_c_normal",
-                })
-                table.insert(opts.sections.lualine_c, {
-                    symbols and symbols.get,
-                    cond = function()
-                        return vim.b.trouble_lualine ~= false and symbols.has()
-                    end,
-                })
-            end
 
             return opts
         end,
